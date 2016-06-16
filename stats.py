1e8c94a6b9d3fbfd45d08197e4bda6f605272c70
Jc Din <ozknightwalker62@gmail.com>
2016-06-16 19:26:30 +0800

    remove comment

--- stats.py
+++ stats.py
@@ -1,9 +1,8 @@
 import pygame
 
 pygame.init()
-pygame.display.set_caption("testing")
-
 myfont = pygame.font.SysFont("monospace", 16)
+
 black = 0, 0, 0
 
 
@@ -14,11 +13,6 @@ def update_score(score, counter=1):
 def update_life(life, counter=1):
     return life + counter
 
-# def draw_stats(score, life):
-#   black = 0, 0, 0
-#   scoretext = myfont.render("Score: {0}".format(score), 1, black)
-#     lifetext = myfont.render("Lives: {0}".format(life), 1, black)
-
 
 def render_score(screen, score):
     x = screen.get_size()[0]
@@ -28,6 +22,6 @@ def render_score(screen, score):
 
 def render_life(screen, life):
     string = 'Life: {}'.format(life)
-    x, y = screen.get_size()
+    x = screen.get_size()[0]
     text = myfont.render(string, 1, black)
     screen.blit(text, ((x / 2), 10))
