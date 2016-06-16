2002aa2edf063e5bc134f219dd495d163ac2a80e
Emmanuel Lodovice <name3anad@gmail.com>
2016-06-16 19:58:02 +0800

    make player move

--- keyboard.py
+++ keyboard.py
@@ -7,15 +7,11 @@ def get_direction_to_move():
     x_dir = 0
     y_dir = 0
     if pressed_keys[K_LEFT]:
-        print 'left'
         x_dir -= 1
     if pressed_keys[K_RIGHT]:
-        print 'right'
         x_dir += 1
     if pressed_keys[K_DOWN]:
-        print 'down'
         y_dir += 1
     if pressed_keys[K_UP]:
-        print 'up'
         y_dir -= 1
     return x_dir, y_dir
--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import sys
 import pygame
 from time import sleep
 from pygame import *
-from stats import *
 
+from stats import *
 from keyboard import get_direction_to_move
+from sprites import PlayerBlock
 pygame.init()
 
 size = width, height = 700, 500
@@ -21,21 +22,27 @@ ballrect = ball.get_rect()
 
 pygame.key.set_repeat(500, 30)
 
+player = PlayerBlock(speed=5, screen_width=width, screen_height=height, image="sample.png")
+player.rect.x = width / 2
+player.rect.y = height / 2
+
+player_group = pygame.sprite.Group()
+player_group.add(player)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
     sleep(0.0166)
-    ballrect = ballrect.move(speed)
     if ballrect.left < 0 or ballrect.right > width:
         speed[0] = -speed[0]
     if ballrect.top < 0 or ballrect.bottom > height:
         speed[1] = -speed[1]
-
     screen.fill(white)
+
+    player_group.draw(screen)
+    player.update_movement(*get_direction_to_move())
+    player_group.update()
     render_life(screen, life)
     render_score(screen, score)
-    screen.blit(ball, ballrect)
     pygame.display.flip()
-
-    print get_direction_to_move()
